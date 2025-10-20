1311a545ff81498b6c5cc04ab82f2fe07828b69f
johnnychiu <johnnychiu0817@gmail.com>
2025-10-20 21:38:03 +0800

    Code

---
+++ 1_star_code/Hashmat the Brave Warrior.cpp
@@ -0,0 +1,12 @@
+//10055 - Hashmat the Brave Warrior
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long int a = 0, b = 0;
+    while (cin >> a >> b){
+        cout << abs(a-b) << '\n';
+    }
+}
+//https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=0&problem=996&mosmsg=Submission+received+with+ID+30742100
